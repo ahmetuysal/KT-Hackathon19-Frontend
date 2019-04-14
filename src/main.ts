3e51bf292d8c53d07f90338a6f222fd2c9d6c06b
Ahmet Uysal <auysal16@ku.edu.tr>
2019-04-14 11:15:36 +0300

    Started adding Turkish Language support

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import 'hammerjs';
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -9,5 +9,13 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+// use the require method provided by webpack
+declare const require;
+// we use the webpack raw-loader to return the content as a string
+const translations = require(`raw-loader!./locale/messages.tr.xlf`);
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    providers: [{ provide: TRANSLATIONS, useValue: translations }, { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' }]
+  })
   .catch(err => console.error(err));
