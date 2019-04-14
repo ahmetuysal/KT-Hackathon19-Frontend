c42080e5b830b4acdf05ed88ee40081837db7009
Ahmet Uysal <auysal16@ku.edu.tr>
2019-04-14 08:00:16 +0300

    Final version of fund creation pages

--- src/app/create-fundraising/create-fundraising.component.ts
+++ src/app/create-fundraising/create-fundraising.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
+import { FileUploadService } from '../shared/services/file-upload.service';
+import { FundraisingService } from '../shared/services/fundraising.service';
+import { FormControl, Validators } from '@angular/forms';
+import { FundraisingPost } from '../shared/models/fundraising-post.model';
 
 @Component({
   selector: 'app-create-fundraising',
@@ -6,10 +11,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-fundraising.component.scss']
 })
 export class CreateFundraisingComponent implements OnInit {
+  image: Blob = null;
+  showCropper: boolean = false;
+  waiting: boolean = false;
+  croppedImage: any;
+  imageChangedEvent: any = '';
 
-  constructor() { }
+  title: FormControl;
+  targetAmount: FormControl;
+  targetIban: FormControl;
+  deadline: FormControl;
+  description: FormControl;
 
-  ngOnInit() {
+  cancelClicked() {
+    this.image = null;
+    this.showCropper = false;
   }
 
+  async doneClicked() {
+    this.showCropper = false;
+  }
+
+  fileChangeEvent(event: any): void {
+    this.showCropper = true;
+    this.imageChangedEvent = event;
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+    this.image = event.file;
+  }
+
+  imageLoaded() {
+    // show cropper
+  }
+
+  cropperReady() {
+    // cropper ready
+  }
+
+  loadImageFailed() {
+    // show message
+  }
+
+  constructor(private fileUploadService: FileUploadService, private FundraisingService: FundraisingService) {}
+
+  ngOnInit() {
+    this.title = new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(256)]);
+    this.targetAmount = new FormControl('', [Validators.required, Validators.min(0)]);
+    this.targetIban = new FormControl('', [Validators.required, Validators.minLength(26), Validators.maxLength(26)]);
+    this.deadline = new FormControl(new Date());
+    this.description = new FormControl('', [Validators.minLength(4), Validators.maxLength(256)]);
+  }
+
+  async submit() {
+    this.waiting = true;
+
+    const newFundraisingPost = new FundraisingPost();
+
+    newFundraisingPost.title = this.title.value;
+    newFundraisingPost.targetAmount = this.targetAmount.value;
+    newFundraisingPost.targetIban = this.targetIban.value;
+    newFundraisingPost.deadline = this.deadline.value;
+    newFundraisingPost.description = this.description.value;
+
+    if (this.image) {
+      const imageUrl = await this.fileUploadService.uploadImage(this.image);
+      console.log(imageUrl);
+      if (imageUrl) newFundraisingPost.image = imageUrl;
+    }
+
+    const success = await this.FundraisingService.createFundraisingPost(newFundraisingPost);
+    if (success) {
+      // Show success
+    } else {
+      // Show error
+    }
+
+    this.waiting = false;
+  }
 }
--- src/app/shared/services/fundraising.service.ts
+++ src/app/shared/services/fundraising.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { FundraisingPost } from '../models/fundraising-post.model';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FundraisingService {
+  constructor(private apiService: ApiService) {}
 
-  constructor() { }
+  async createFundraisingPost(raisingPost: FundraisingPost): Promise<boolean> {
+    const result = await this.apiService.post('fundraising-post', { fundraisingPost: raisingPost }).toPromise();
+    if (result.StatusToken && result.StatusToken == 201) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
